ac7ed8cc517788d20f097a232a14d09ccfd04c04
Tommaso <torti.tommaso@gmail.com>
2011-03-09 12:42:07 +0100

    added pdf

--- script/server.sh
+++ script/server.sh
@@ -2,10 +2,11 @@
 project_root="$(dirname $0)/.."
 command=${1:-run}
 
-export CATALINA_BASE="$project_root/tomcat-stuff"
+export JPROFILER_HOME="/Users/tommasotorti/Documents/workspace-articolo-performance/jprofiler/jprofiler6"
 export CATALINA_HOME="/Users/tommasotorti/Documents/workspace-articolo-performance/tomcat/apache-tomcat-6.0.32"
 
-export CATALINA_OPTS="-agentpath:/Users/tommasotorti/Documents/workspace-articolo-performance/jprofiler/jprofiler6/bin/macos/libjprofilerti.jnilib=port=8849 $CATALINA_OPTS"
+export CATALINA_BASE="$project_root/tomcat-stuff"
+export CATALINA_OPTS="-agentpath:"$JPROFILER_HOME"/bin/macos/libjprofilerti.jnilib=port=8849 $CATALINA_OPTS"
 
 
 "$CATALINA_HOME/bin/catalina.sh" $command
